fdabd98758ad06569bbf28bc339753cb3f7b5fc6
igris27 <andreydeivi.@gmail.com>
2025-11-28 15:57:55 -0500

    añadiendo basename que es una ruta en react para git hub pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/tuso-demo-web/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
